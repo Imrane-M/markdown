95f448ffcaa5e97ca50ce1bfcaa71ac1b88c7ecc
Yann N'DA <yannnda97@gmail.com>
2024-10-28 19:23:33 +0100

    Dashboard - done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import Citation from "./components/Citation";
-import Blague from "./components/Blague";
-import EvenementsHistoriques from "./components/EvenementsHistoriques";
-import MocktailDuJour from "./components/Mocktails";
-import RecetteDuJour from "./components/Recettes";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 import Editor from "./pages/Editor";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>-----------------------------------------------------------------------</p>
-      <Citation/>
-      <p>-----------------------------------------------------------------------</p>
-      <Blague/>
-      <p>-----------------------------------------------------------------------</p>
-      <EvenementsHistoriques/>
-      <p>-----------------------------------------------------------------------</p>
-      <MocktailDuJour/>
-      <p>-----------------------------------------------------------------------</p>
-      <RecetteDuJour/>
-      <p>-----------------------------------------------------------------------</p>
-      <Editor />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <br />
+        <Link to="/editor">Editor</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/editor" element={<Editor />}/>
+      </Routes>
+    </Router>
   );
 }
 
